d88ae34789af6d8b9ee8e4d01f424a8a0f48e255
charmas3r <evansmith0115@gmail.com>
2019-06-23 00:40:44 -0700

    navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,36 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AboutComponent} from './about/about.component';
+import { PanelsComponent} from './panels/panels.component';
+import { ResourcesComponent} from './resources/resources.component';
+import {ContactComponent} from './contact/contact.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'panels',
+    component: PanelsComponent
+  },
+  {
+    path: 'resources',
+    component: ResourcesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
