25d12736df6e6e3a843bb74be29fd98461e5fb6b
charmas3r <evansmith0115@gmail.com>
2019-06-22 20:53:37 -0700

    full screen home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,22 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { PanelsComponent } from './panels/panels.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PanelsComponent,
+    ResourcesComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
