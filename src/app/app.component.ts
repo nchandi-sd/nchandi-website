cd8618176474591d7ad540bfdab96f9f052d6489
charmas3r <evansmith0115@gmail.com>
2019-07-14 11:12:12 -0700

    footer ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  footer: boolean;
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  // The key here is that we have use the Router module and check what link we are currently
+  // on. We set a boolean varianle true if we are on any page except home and false if it is
+  // on home. Then in HTML we can use ngif to show the component based on the boolean variable.
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.footer = (event.url !== '/home');
+        }
+      });
+  }
 }
