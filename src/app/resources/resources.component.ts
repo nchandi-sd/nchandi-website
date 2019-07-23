f9732291baac92aba507546c0693218cd6798ea1
charmas3r <evansmith0115@gmail.com>
2019-07-22 22:11:21 -0700

    general design

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -3,6 +3,7 @@ import {MONTHLY_REPORTS} from '../model/Monthly-Reports';
 import {CommitteeReport} from '../model/CommitteeReport';
 import {GENERAL_RESOURCES} from '../model/General-Resources';
 import {Resource} from '../model/Resource';
+import {PANEL_MATERIALS} from '../model/Panel-Materials';
 
 
 @Component({
@@ -12,19 +13,25 @@ import {Resource} from '../model/Resource';
 })
 export class ResourcesComponent implements OnInit {
 
-  summerReports: Array<CommitteeReport> = [];
-  winterReports: Array<CommitteeReport> = [];
+  firstTriReports: Array<CommitteeReport> = [];
+  secTriReports: Array<CommitteeReport> = [];
+  thirdTriReports: Array<CommitteeReport> = [];
+
   generalResources: Array<Resource> = GENERAL_RESOURCES;
+  panelMaterials: Array<Resource> = PANEL_MATERIALS;
 
   constructor() { }
   ngOnInit() {
 
     // first six reports to be added to the first column
-    for (let i = 0; i < 6; i++) {
-      this.summerReports.push(MONTHLY_REPORTS[i]);
+    for (let i = 0; i < 4; i++) {
+      this.firstTriReports.push(MONTHLY_REPORTS[i]);
     }
-    for (let i = 6; i < 12; i++) {
-      this.winterReports.push(MONTHLY_REPORTS[i]);
+    for (let i = 4; i < 8; i++) {
+      this.secTriReports.push(MONTHLY_REPORTS[i]);
+    }
+    for (let i = 8; i < 12; i++) {
+      this.thirdTriReports.push(MONTHLY_REPORTS[i]);
     }
   }
 
