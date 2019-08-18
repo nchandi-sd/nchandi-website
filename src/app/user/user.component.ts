7c539037b440fc12de2ebb4aafec474d3583808d
charmas3r <evansmith0115@gmail.com>
2019-08-17 19:50:57 -0700

    cms change

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { UserService } from '../core/user.service';
 import { AuthService } from '../core/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseUserModel } from '../core/user.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   templateUrl: 'user.component.html',
   styleUrls: ['user.component.scss']
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
 
   user: FirebaseUserModel = new FirebaseUserModel();
   profileForm: FormGroup;
+  fileData: File = null;
+  public name: any = 'Choose file';
+  public str: any;
 
   constructor(
     public userService: UserService,
     public authService: AuthService,
     private route: ActivatedRoute,
-    private location : Location,
-    private fb: FormBuilder
+    private location: Location,
+    private fb: FormBuilder,
+    private http: HttpClient
   ) {
 
   }
@@ -38,23 +43,39 @@ export class UserComponent implements OnInit{
 
   createForm(name) {
     this.profileForm = this.fb.group({
-      name: [name, Validators.required ]
+      name: [name, Validators.required]
     });
   }
 
-  save(value){
-    this.userService.updateCurrentUser(value)
-    .then(res => {
-      console.log(res);
-    }, err => console.log(err))
-  }
-
-  logout(){
+  logout() {
     this.authService.doLogout()
-    .then((res) => {
-      this.location.back();
-    }, (error) => {
-      console.log('Logout error', error);
-    });
+      .then((res) => {
+        this.location.back();
+      }, (error) => {
+        console.log('Logout error', error);
+      });
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post('url/to/your/api', formData)
+      .subscribe(res => {
+        console.log(res);
+        alert('SUCCESS !!');
+      });
+  }
+
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+    console.log(this.fileData.name);
+    this.name = this.fileData.name;
+  }
+
+  onFileChange(event) {
+    this.fileData = event.target.files[0];
+    this.name = this.fileData.name;
   }
 }
+
+
