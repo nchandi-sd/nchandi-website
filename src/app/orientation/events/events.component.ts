f617d6ae0f5455856178d7a9efb1fb9131adcb0a
Evan Smith <evansmith0115@gmail.com>
2020-02-22 12:56:09 -0800

    Events fix space between sob req and rest

--- src/app/orientation/events/events.component.ts
+++ src/app/orientation/events/events.component.ts
@@ -23,6 +23,48 @@ export class EventsComponent implements OnInit {
   ngOnInit() {
     this.eventClicked.emit(this.currentEvent);
     console.log('First Event is ' + this.currentEvent.name);
+    console.log('number of events ' + this.events.length);
+    this.filterEvents();
+
+  }
+
+  // Quick fix to make the page look better if more than 6 dates are showing (It pushes down the "Sobriety Requirements...)
+  filterEvents() {
+    switch (this.events.length) {
+      case 12: {
+        const toBeRemoved = this.events.length - 6;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      case 11: {
+        const toBeRemoved = this.events.length - 5;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      case 10: {
+        const toBeRemoved = this.events.length - 4;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      case 9: {
+        const toBeRemoved = this.events.length - 3;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      case 8: {
+        const toBeRemoved = this.events.length - 2;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      case 7: {
+        const toBeRemoved = this.events.length - 1;
+        this.events = this.events.splice(0, toBeRemoved);
+        break;
+      }
+      default: {
+        console.log('No meetings to remove from queue ' + this.currentEvent.name);
+      }
+    }
   }
 
   onClick(event: Event): void {
