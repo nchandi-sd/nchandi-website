bca3cbd2b7783bafb59e53827070ae19deac6183
Evan Smith <evansmith0115@gmail.com>
2020-02-24 23:49:01 -0800

    ContactPage submit fix

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
-import { Contact } from '../model/Contact';
-import {ContactService} from '../contact/contact.service'
+import {Component, ElementRef, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators, NgForm} from '@angular/forms';
+import {Contact} from '../model/Contact';
+import {ContactService} from '../contact/contact.service';
+import {timer} from 'rxjs';
+import {isCombinedNodeFlagSet} from 'tslint';
 
 
 @Component({
@@ -10,12 +12,18 @@ import {ContactService} from '../contact/contact.service'
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  @ViewChildren('text') textBoxes: QueryList<ElementRef>;
+
   registered = false;
   submitted = false;
   userForm: FormGroup;
   contact: Contact;
+  sendDisabled = false;
+  contactSubmitted = false;
 
-  constructor(private formBuilder: FormBuilder, private contactService: ContactService) {}
+
+  constructor(private formBuilder: FormBuilder, private contactService: ContactService) {
+  }
 
   invalidFirstName() {
     return (this.submitted && this.userForm.controls.first_name.errors != null);
@@ -38,13 +46,13 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.contact = new Contact()
+    this.contact = new Contact();
     this.userForm = this.formBuilder.group({
       first_name: ['', Validators.required],
       last_name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern('^(1?-?[(]?(-?\\d{3})[)]?-?)?(\\d{3})(-?\\d{4})$')]],
-      message: ['',[Validators.required]]
+      message: ['', [Validators.required]]
     });
   }
 
@@ -55,17 +63,37 @@ export class ContactComponent implements OnInit {
     } else {
       this.registered = true;
     }
-    this.contact.email = this.userForm.controls.email.value
-    this.contact.firstName = this.userForm.controls.first_name.value
-    this.contact.lastName = this.userForm.controls.last_name.value
-    this.contact.phone = this.userForm.controls.phone.value
-    this.contact.message = this.userForm.controls.message.value
+    this.contact.email = this.userForm.controls.email.value;
+    this.contact.firstName = this.userForm.controls.first_name.value;
+    this.contact.lastName = this.userForm.controls.last_name.value;
+    this.contact.phone = this.userForm.controls.phone.value;
+    this.contact.message = this.userForm.controls.message.value;
 
-    this.postContactForm(form.value)
+    this.sendDisabled = true;
+    this.postContactForm(form.value);
+  }
+
+  timerToClear(form: NgForm) {
+    const source = timer(1000, 2000);
+    const subscribe = source.subscribe(val => {
+      console.log(val);
+      this.textBoxes.forEach((element) => {
+        element.nativeElement.value = '';
+      });
+      // clears the submit message after 3 seconds
+      if (val === 3) {
+        this.contactSubmitted = false;
+        subscribe.unsubscribe();
+      }
+    });
+  }
+
+  clearForm(form: NgForm) {
+    console.log(form.control.value.phone);
   }
 
   postContactForm(form: NgForm) {
-    console.log("contact: "+ this.contact)
+    console.log('contact: ' + this.contact);
 
     // let contactUrl = 'https://script.google.com/macros/s/AKfycbzezNitOBOTReBZ7kvtV8fzTEiW-bA8mGOnJDQl7orAr65gvRd8/exec';
     // var xhr = new XMLHttpRequest();
@@ -81,7 +109,12 @@ export class ContactComponent implements OnInit {
     // };
     // xhr.send(JSON.stringify(form));
 
-     this.contactService.postContactForm(this.contact)
-       .subscribe(() => console.log("yay subscribe called"))
+    this.contactService.postContactForm(this.contact)
+      .subscribe(() => {
+        this.sendDisabled = false;
+        this.contactSubmitted = true;
+        console.log('Contact form successfully sent');
+        this.timerToClear(form);
+      });
   }
 }
