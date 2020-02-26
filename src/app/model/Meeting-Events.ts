1989e90dc8e491c5bf7fc157bbf05911d1f57c3a
Evan Smith <evansmith0115@gmail.com>
2020-02-25 20:59:06 -0800

    OrientationPage Remove filtering of dates

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -5,10 +5,5 @@ export const EVENTS: Event[] = [
   {id: 2, name: 'April 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
   {id: 3, name: 'May 16th, 2020', type: 'Committee Meeting', date: '9:00am'},
   {id: 4, name: 'June 20th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'July 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'August 15th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'September 19th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'October 17th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'November 21st, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'December 19th, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 5, name: 'July 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
 ];
