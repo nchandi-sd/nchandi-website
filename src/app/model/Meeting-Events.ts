94e33b675b27d9b2d483579c9f1b04bcc7f75085
Evan Smith <evansmith0115@gmail.com>
2020-04-18 09:41:01 -0700

    EventDate hotfix to master

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -1,8 +1,6 @@
 import { Event } from './Event';
 
 export const EVENTS: Event[] = [
-  {id: 1, name: 'March 21st, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 2, name: 'April 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
   {id: 3, name: 'May 16th, 2020', type: 'Committee Meeting', date: '9:00am'},
   {id: 4, name: 'June 20th, 2020', type: 'Committee Meeting', date: '9:00am'},
   {id: 5, name: 'July 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
