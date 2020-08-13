5d05b2cb5bd177ae4192e39d1c26785e3f1787dc
michaeltoddhoward <elcajonca@yahoo.com>
2020-08-13 05:14:50 -0700

    UpdatedMeetingDatesAddedPayPal

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -1,7 +1,9 @@
 import { Event } from './Event';
 
 export const EVENTS: Event[] = [
-  {id: 3, name: 'May 16th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'June 20th, 2020', type: 'Committee Meeting', date: '9:00am'},
-  {id: 5, name: 'July 18th, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 3, name: 'August 15th, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 4, name: 'September 19th, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 5, name: 'October 17th, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 6, name: 'November 21st, 2020', type: 'Committee Meeting', date: '9:00am'},
+  {id: 7, name: 'December 19th, 2020', type: 'Committee Meeting', date: '9:00am'}
 ];
