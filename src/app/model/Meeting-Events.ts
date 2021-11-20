4599ea955c1fc4f16cd2f9e91cb989c462077020
michaeltoddhoward <elcajonca@yahoo.com>
2021-11-20 05:46:06 -0800

    Updated Committee Meeting Dates

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -1,9 +1,9 @@
 import { Event } from './Event';
 
 export const EVENTS: Event[] = [
-  {id: 3, name: 'July 17th, 2021', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'August 21st, 2021', type: 'Committee Meeting', date: '9:00am'},
-  {id: 5, name: 'September 18th, 2021', type: 'Committee Meeting', date: '9:00am'},
-  {id: 6, name: 'October 16th, 2021', type: 'Committee Meeting', date: '9:00am'},
-  {id: 7, name: 'November 20th, 2021', type: 'Committee Meeting', date: '9:00am'}
+  {id: 3, name: 'December 18, 2021', type: 'Committee Meeting', date: '9:00am'},
+  {id: 4, name: 'January 15, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 5, name: 'Feburary 19, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 6, name: 'March 19, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 7, name: 'April 16, 2022', type: 'Committee Meeting', date: '9:00am'}
 ];
