b7d4453e7e1d5e6b83e894094943172235c33d65
michaeltoddhoward <elcajonca@yahoo.com>
2022-04-02 05:33:34 -0700

    remove david p and update dates

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -1,9 +1,9 @@
 import { Event } from './Event';
 
 export const EVENTS: Event[] = [
-  {id: 3, name: 'December 18, 2021', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'January 15, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 5, name: 'Feburary 19, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 6, name: 'March 19, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 7, name: 'April 16, 2022', type: 'Committee Meeting', date: '9:00am'}
+  {id: 0, name: 'April 16, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 1, name: 'May 21, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 2, name: 'June 18, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 3, name: 'July 16, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 4, name: 'August 20, 2022', type: 'Committee Meeting', date: '9:00am'}
 ];
