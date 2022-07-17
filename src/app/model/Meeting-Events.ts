9db18d9a214aeea0d1cb5c867b55798e8de4621b
michaeltoddhoward <elcajonca@yahoo.com>
2022-07-17 06:40:45 -0700

    Add Emails To Contact Page

--- src/app/model/Meeting-Events.ts
+++ src/app/model/Meeting-Events.ts
@@ -1,9 +1,9 @@
 import { Event } from './Event';
 
 export const EVENTS: Event[] = [
-  {id: 0, name: 'April 16, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 1, name: 'May 21, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 2, name: 'June 18, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 3, name: 'July 16, 2022', type: 'Committee Meeting', date: '9:00am'},
-  {id: 4, name: 'August 20, 2022', type: 'Committee Meeting', date: '9:00am'}
+  {id: 0, name: 'August 20, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 1, name: 'September 17, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 2, name: 'October 15, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 3, name: 'November 19, 2022', type: 'Committee Meeting', date: '9:00am'},
+  {id: 4, name: 'December 17, 2022', type: 'Committee Meeting', date: '9:00am'}
 ];
